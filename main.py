fad2a3330d22468afcbfcae9039e3f3e20915247
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-31 04:47:55 +0000

    Add CLI commands for learning and merging code

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import platform
 import os
 
 
+#! I will add all the factors in which are code can be created
 def get_operating_system():
     """Get the name of the operating system"""
     os_name = platform.system()
     return os_name
+
 
 
 def does_system_has_git():
@@ -65,47 +67,59 @@ def activate_project():
     else:
         # Need to improve too much here
         subprocess.call(['.venv/Scripts/activate'])
-    
 
-def learn_more_about_your_project():
-    newBranchName = input("Enter the branch name: ")
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--name', prompt='Enter the branch name', help='The name of the new branch you want to learn for this project')
+def learn_more_about_your_project(newBranchName: str):
+    """
+    Create a new branch, install Jupyter Notebook, and create a new Jupyter Notebook file.
+
+    Args:
+        newBranchName (str): The name of the new branch.
+
+    Returns:
+        None
+    """
     subprocess.run(["git", "checkout", "-b", "learn"+newBranchName])
     subprocess.run(["pip", "install", "jupyter", "notebook"])
     subprocess.run(["touch", newBranchName+".ipynb"])
 
 
-def merge_to_your_branch():
+@click.command()
+def merge_the_learn_code():
+    """
+    Merge the changes from the current branch to the production branch.
+
+    This function checks if the current branch contains the word "learn" in its name.
+    If it does, it removes the "learn" prefix from the branch name, uninstalls the "jupyter" and "notebook" packages,
+    deletes the corresponding notebook file, checks out the "production" branch, pulls the latest changes from the remote "production" branch,
+    merges the changes from the learning branch to the "production" branch, and finally deletes the learning branch.
+
+    If the current branch does not contain the word "learn" in its name, it displays a message indicating that it is not a learning branch.
+    """
     currentBranch = subprocess.run(["git", "branch", "--show-current"], capture_output=True, text=True).stdout.strip()
-
-    if(currentBranch == "production"):
-        print("You are in production branch. Please switch to learn branch")
-        exit()
-
     if("learn" in currentBranch):
         currentBranch = currentBranch[5:]
+        subprocess.run(["pip", "uninstall", "jupyter", "notebook"])
+        subprocess.run(["rm", currentBranch+".ipynb"])
+        subprocess.run(["git", "checkout", "production"])
+        subprocess.run(["git", "pull", "origin", "production"])
 
-    print("Current Branch: ", currentBranch)
+        subprocess.run(["git", "merge", "learn"+currentBranch])
+        subprocess.run(["git", "branch", "-d", "learn"+currentBranch])
 
+    else:
+        click.echo("You are not in a learning branch")
+    
 
-    subprocess.run(["pip", "uninstall", "jupyter", "notebook"])
-
-    # delete the currentBranch.ipynb file
-    subprocess.run(["rm", currentBranch+".ipynb"])
-    subprocess.run(["git", "checkout", "production"])
-    subprocess.run(["git", "pull", "origin", "production"])
-
-    result = subprocess.run(["git", "merge", "learn"+currentBranch])
-
-
-    print("End of the automation script:    ")
+cli.add_command(learn_more_about_your_project)
+cli.add_command(merge_the_learn_code)
 
 
 if __name__ == '__main__':
-    get_operating_system()
-def project_created_for_python():
-    """Check if the project is created for python"""
-    r = does_system_has_python()
-    if(r == "python"):
-        return True
-    else:
-        return False
+    cli()
